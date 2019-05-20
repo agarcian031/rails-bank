cbbfc31494c995a6a19c01c062e8e779b8afbdc4
Amanda Garcia <algarcian@outlook.com>
2019-05-20 11:20:15 -0600

    Account Index changes

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,5 +1,6 @@
 class AccountsController < ApplicationController
   def index
+    current_user #will return the object of the user that is currently logged in | it is a devise helper method 
   end
 
   def show
